c12546362a908ce3cde08157502064c97b03bbd2
odysei <odysei@gmail.com>
2017-11-07 16:45:09 +0100

    Some fixes for building tmux

--- install/tmux/run_me.sh
+++ install/tmux/run_me.sh
@@ -12,36 +12,53 @@ if [ ! -f ${PARENT_PATH}/make_install_libevent.sh ] && \
    [ ! -f ${PARENT_PATH}/make_install_ncurses.sh ] && \
    [ ! -f ${PARENT_PATH}/make_install_tmux.sh ]; then
     echo "Helper scripts not found!"
-    return
+    return 1
 fi
 
 #
 #   Functions
 #
-Download_src()
+Download_src_libevent()
 {
-    echo "Getting the needed archives"
     if [ ! -f ${LIBEVENT_ARCH_NAME} ]; then
         wget ${LIBEVENT_ARCH_PATH}/${LIBEVENT_ARCH_NAME}
     fi
+}
 
+Download_src_ncurses()
+{
     if [ ! -f ${NCURSES_ARCH_NAME} ]; then
         wget ${NCURSES_ARCH_PATH}/${NCURSES_ARCH_NAME}
-    fi 
+    fi
+}
 
+Download_src_tmux()
+{
     if [ ! -f ${TMUX_ARC_NAME} ]; then
         wget ${TMUX_ARC_PATH}/${TMUX_ARC_NAME}
     fi
+}
+
+Download_src()
+{
+    echo "Getting the needed archives"
+    
+    Download_src_libevent
+    Download_src_ncurses
+    Download_src_tmux
 }
 
 Extract_src()
 {
     echo "Extracting src"
     echo "Install location: ${INSTALL_LOCATION}"
+    
+    echo "Creating ${INSTALL_LOCATION}/src"
     mkdir -p ${INSTALL_LOCATION}/src
-    echo ${INSTALL_LOCATION}/src
+    
     cd ${INSTALL_LOCATION}/src
 
+    # extract inside of target src/
     tar -xf ${CURRENT_DIR}/${LIBEVENT_ARCH_NAME}
     tar -xf ${CURRENT_DIR}/${NCURSES_ARCH_NAME}
     tar -xf ${CURRENT_DIR}/${TMUX_ARC_NAME}
@@ -49,44 +66,63 @@ Extract_src()
     if [ ! -e ${NCURSES_ARCH_NAME_} ]; then
         echo "Whoops! Made for ${NCURSES_ARCH_NAME_}. Update run_me_sett.sh!"
         cd ${CURRENT_DIR}
-        return
+        return 1
     fi
+    cd ${CURRENT_DIR}
 }
 
-Build_install()
+Build_install_libevent()
 {
-    echo "Building stuff (log file: ${LOG_FILE})"
-    cd ${INSTALL_LOCATION}/src
+    cd ${INSTALL_LOCATION}/src/${LIBEVENT_ARCH_NAME_}
     
-    cd ${LIBEVENT_ARCH_NAME_}
-    . ${PARENT_PATH}/make_install_libevent.sh ${NCORES} >> \
+    bash ${PARENT_PATH}/make_install_libevent.sh ${NCORES} >> \
         ${CURRENT_DIR}/${LOG_FILE} 2>&1
     if [ ! $? -eq 0 ]; then
         echo -e "Build error in ${LIBEVENT_ARCH_NAME_}. See ${LOG_FILE}"
         cd ${CURRENT_DIR}
         return 1
     fi
+    
+    cd ${CURRENT_DIR}
+}
 
-    cd ../${NCURSES_ARCH_NAME_}
-    . ${PARENT_PATH}/make_install_ncurses.sh ${NCORES} >> \
+Build_install_ncurses()
+{
+    cd ${INSTALL_LOCATION}/src/${NCURSES_ARCH_NAME_}
+
+    bash ${PARENT_PATH}/make_install_ncurses.sh ${NCORES} >> \
         ${CURRENT_DIR}/${LOG_FILE} 2>&1
     if [ ! $? -eq 0 ]; then
         echo -e "Build error in ${NCURSES_ARCH_NAME_}. See ${LOG_FILE}"
         cd ${CURRENT_DIR}
         return 1
     fi
+    
+    cd ${CURRENT_DIR}
+}
 
-    cd ${TMUX_ARC_NAME_}
-    cd ../${TMUX_ARC_NAME_}
-    . ${PARENT_PATH}/make_install_tmux.sh ${NCORES} >> \
+Build_install_tmux()
+{
+    cd ${INSTALL_LOCATION}/src/${TMUX_ARC_NAME_}
+
+    bash ${PARENT_PATH}/make_install_tmux.sh ${NCORES} >> \
         ${CURRENT_DIR}/${LOG_FILE} 2>&1
     if [ ! $? -eq 0 ]; then
         echo -e "Build error in ${TMUX_ARC_NAME_}. See ${LOG_FILE}"
         cd ${CURRENT_DIR}
         return 1
     fi
-
+    
     cd ${CURRENT_DIR}
+}
+
+Build_install()
+{
+    echo "Building stuff (log file: ${LOG_FILE})"
+    
+    Build_install_libevent
+    Build_install_ncurses
+    Build_install_tmux
 }
 
 
