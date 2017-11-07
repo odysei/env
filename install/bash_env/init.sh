84d2b0bef1aecd7933c4092c30b657b0d9fc7c2b
odysei <odysei@gmail.com>
2017-11-07 18:08:11 +0100

    Force

--- install/bash_env/init.sh
+++ install/bash_env/init.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ${HOME}/.bash
 
-rm ${HOME}/.bashrc
-rm ${HOME}/.bash_profile
+rm -f ${HOME}/.bashrc
+rm -f ${HOME}/.bash_profile
 
 ln -s ${HOME}/.bash/rc ${HOME}/.bashrc
 ln -s ${HOME}/.bash/rc ${HOME}/.bash_profile
